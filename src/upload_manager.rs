73a94f4103835ff53e9dfd79c64778d11cc3f1ec
Stuart Hunter <stuart.hunter@comcast.net>
2026-02-15 09:12:15 -0500

    code headers updated - no code changes

--- src/upload_manager.rs
+++ src/upload_manager.rs
@@ -1,3 +1,27 @@
+/*
+ *  upload_manager.rs
+ *
+ *  dbxup - simple dropbox sync service
+ *      (c) 2025-26 Stuart Hunter
+ *
+ *  Independent astronomical calculations (sunrise, sunset, moonrise, moonset)
+ *  Used for auto-brightness and display - works without weather service
+ *
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      See <http://www.gnu.org/licenses/> to get a copy of the GNU General
+ *      Public License.
+ *
+ */
+
 use crate::dropbox::client::DropboxClient;
 use crate::dropbox::upload;
 use crate::error::Result;
